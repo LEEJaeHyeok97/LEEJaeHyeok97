cdd2218c250bdc56973ccaedb617f3d886ad31d8
이재혁 <hazardous10@naver.com>
2025-03-30 14:56:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ markdown_text = """
 ## Hi, I'm JaeHyeok Lee, a dev writing backend developer! passionate about making engaging experiences
 """  # list of blog posts will be appended here
 
+boj_tier = """
+## [![](https://mazassumnida.wtf/api/mini/generate_badge?boj=hazardous10)](https://solved.ac/hazardous10/)
+![](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fohksj77&count_bg=%2329B0C6&title_bg=%23434343&icon=&icon_color=%23E7E7E7&title=&edge_flat=false)
+"""
+
 for idx, feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
@@ -17,8 +22,8 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         
 f = open("README.md", mode="w", encoding="utf-8")
 f.write(markdown_text)
+f.write(boj_tier)
 f.close()
 
 
-[![](https://mazassumnida.wtf/api/mini/generate_badge?boj=hazardous10)](https://solved.ac/hazardous10/)
-![](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fohksj77&count_bg=%2329B0C6&title_bg=%23434343&icon=&icon_color=%23E7E7E7&title=&edge_flat=false)
+
